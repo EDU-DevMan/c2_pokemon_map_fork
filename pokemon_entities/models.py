4da377c707a169a928c55119f046647ccdc0be7e
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-07-18 18:26:51 +0300

    replaced name pokemon_names on pokemons for related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -29,7 +29,7 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon, verbose_name='Название покемона',
         on_delete=models.CASCADE,
-        related_name='pokemon_names',)
+        related_name='pokemons',)
 
     lat = models.FloatField(verbose_name='Широта',)
 
